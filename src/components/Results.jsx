a7b574d9ec75a4007b7f3c72e31815a8fc4edeb4
Victor <victorcorreaa95@gmail.com>
2024-02-21 17:16:03 -0300

    Add: calculation functionality to the Results component.

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,7 @@
+import { calculateInvestmentResults } from '../util/investment.js' 
+
 export default function Results({ input }){
-    console.log(input)
+    const resultsData = calculateInvestmentResults(input);
+    console.log(resultsData)
     return <p>Results...</p>
 }
